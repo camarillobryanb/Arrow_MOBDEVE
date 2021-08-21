de615cc75d02efd4960450a78ce457d38cb64190
camarillobryanb <bryan_daniel_camarillo@dlsu.edu.ph>
2021-08-21 17:21:18 +0800

    Onboard 1-3

---
+++ app/src/main/java/com/example/arrow/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.arrow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button tvContinue;
+    private Button tvContinue2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.initComponents();
+    }
+
+    private void initComponents() {
+        this.tvContinue = findViewById(R.id.btn_continue);
+        this.tvContinue.setOnClickListener(v -> setContentView(R.layout.activity_main2));
+    }
+}

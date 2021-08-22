04b4cb233479384a3e236faaf321aac766eb9ffb
camarillobryanb <bryan_daniel_camarillo@dlsu.edu.ph>
2021-08-22 19:31:01 +0800

    register page sample

--- app/src/main/java/com/example/arrow/MainActivity.java
+++ app/src/main/java/com/example/arrow/MainActivity.java
@@ -8,18 +8,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button tvContinue;
-    private Button tvContinue2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.initComponents();
-    }
-
-    private void initComponents() {
-        this.tvContinue = findViewById(R.id.btn_continue);
-
     }
 }

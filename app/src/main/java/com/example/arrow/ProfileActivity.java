dc2c187a023d18205f436b915cbefb99f22869de
camarillobryanb <bryan_daniel_camarillo@dlsu.edu.ph>
2021-08-26 15:04:37 +0800

    profile db pull

--- app/src/main/java/com/example/arrow/ProfileActivity.java
+++ app/src/main/java/com/example/arrow/ProfileActivity.java
@@ -1,18 +1,26 @@
 package com.example.arrow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -24,10 +32,22 @@ public class ProfileActivity extends AppCompatActivity {
     ImageView iv_home;
     TextView tvLogout;
     TextView tvFeatProf;
+    TextView tvFname;
+    TextView tvLname;
+
+    // Counts
+    private int featuredCount = 0;
+    private ArrayList<String> allFeatured = new ArrayList<String>();
+    private int currentCount = 0;
+    private ArrayList<String> allCurrent = new ArrayList<String>();
 
     // Firebase
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
+
+    // Views
+    ArrayList<MyCardHelperClass> dataProfs = new ArrayList<>();
+    ArrayList<MyCardHelperClass> dataProfsCurr = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +56,11 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         initFirebase();
+
+        tvFname = findViewById(R.id.profile_firstName);
+        tvLname = findViewById(R.id.profile_lastName);
+
+        changeHeading();
 
         rvFeaturedProfs = findViewById(R.id.myFeat_Recycler);
         rvCurrentProfs = findViewById(R.id.myCurr_Recycler);
@@ -57,7 +82,22 @@ public class ProfileActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
 
+    private void changeHeading() {
+        database.getReference().child("users").child(mAuth.getUid())
+                .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DataSnapshot snapshot = task.getResult();
+                    String fname = String.valueOf(snapshot.child("fName").getValue());
+                    String lname = String.valueOf(snapshot.child("lName").getValue());
+                    tvFname.setText(fname);
+                    tvLname.setText(lname);
+                }
+            }
+        });
     }
 
     private void viewAddFeaturedPage() {
@@ -77,17 +117,75 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     private void displayMyFeaturedProfessors() {
+        getFeaturedCount();
+    }
+
+    private void getFeaturedCount() {
+        database.getReference().child("users").child(mAuth.getUid()).child("featured")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Log.d("FIREBASE", ""+snapshot.getChildrenCount());
+                        featuredCount = (int) snapshot.getChildrenCount();
+                        getFeatured();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void getFeatured() {
+        Log.d("FIREBASE", ""+featuredCount);
+        DatabaseReference tempdb = database.getReference().child("users").child(mAuth.getUid()).child("featured");
+
+        for (int i = 0; i < featuredCount; i++){
+            tempdb.child(Integer.toString(i))
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String value = snapshot.getValue().toString();
+                            allFeatured.add(value);
+                            Log.d("ALL FEATURED", ""+allFeatured);
+                            if (allFeatured.size() == featuredCount){
+                                displayFeatured();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+        }
+    }
+
+    private void displayFeatured() {
         rvFeaturedProfs.setHasFixedSize(true);
         rvFeaturedProfs.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
 
-        ArrayList<MyCardHelperClass> dataProfs = new ArrayList<>();
+        for (int i = 0; i < featuredCount; i++){
+            database.getReference().child("professors").child(allFeatured.get(i))
+                    .get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (task.isSuccessful()){
+                        DataSnapshot snapshot = task.getResult();
 
-        dataProfs.add(new MyCardHelperClass("Mrs. Santos", "Team Sports Professor" , R.drawable.prof_sample));
-        dataProfs.add(new MyCardHelperClass("Mrs. Cruz", "IT Professor", R.drawable.prof_sample));
-        dataProfs.add(new MyCardHelperClass("Mr. Perez", "History Professor", R.drawable.prof_sample));
+                        String pronoun = String.valueOf(snapshot.child("pronoun").getValue());
+                        String lname = String.valueOf(snapshot.child("lName").getValue());
+                        String college = String.valueOf(snapshot.child("college").getValue());
 
-        adapter = new MyProfsAdapter(dataProfs);
-        rvFeaturedProfs.setAdapter(adapter);
+                        dataProfs.add(new MyCardHelperClass(pronoun + " " + lname, college, R.drawable.prof_sample));
+
+                        adapter = new MyProfsAdapter(dataProfs);
+                        rvFeaturedProfs.setAdapter(adapter);
+                    }
+                }
+            });
+        }
     }
 
     private void viewHome() {
